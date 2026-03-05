cc25a07cc7a09b23812c428d0f6ce7328851c900
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-05 09:55:25 +0530

    cfer

--- pass_by_obj_reference.py
+++ pass_by_obj_reference.py
@@ -1,10 +1,12 @@
 def immutable_func(x):
-    x = ("modified","new one")
+    x = ("modified", "new one")
     print(f"Inside immutable_func: {x}")
+
 
 def mutable_func(val):
     val = {"role": "modified developer"}
     print(f"Inside mutable_func: {val}")
+
 
 a = ("original",)
 immutable_func(a)
