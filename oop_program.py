b13594e25d50305d4d5b9d0a1dea02fb782fe3ce
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-07 18:33:48 +0530

    cmd2

--- oop_program.py
+++ oop_program.py
@@ -13,3 +13,20 @@ p1 = Person("Kajal", 36)
 
 # Call the greet method
 p1.greet()
+
+
+# Create another class for init method
+#  Create the Dog class
+class Dog:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def bark(self):
+    print(self.name + " says Woof!")
+
+# Create an object
+d1 = Dog("Buddy", 3)
+
+# Call the bark method
+d1.bark()
