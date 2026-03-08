9f92c3e4c2ecd6ed810c1c1bbc3d7e7d029d1da9
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-08 19:46:25 +0530

    cm1

--- pratice_que.py
+++ pratice_que.py
@@ -1,35 +1,51 @@
-# q.1
-numbers = [1, 2, 3, 4, 5]
+# # q.1
+# numbers = [1, 2, 3, 4, 5]
 
-# list comprehension
-squares = [x**2 for x in numbers]
+# # list comprehension
+# squares = [x**2 for x in numbers]
 
-# dictionary comprehension
-square_dict = {x: x**2 for x in numbers}
+# # dictionary comprehension
+# square_dict = {x: x**2 for x in numbers}
 
-print(squares)
-print(square_dict)
-
-
-# q.2
-def check_even(func):
-    def wrapper(*args, **kyargs):
-        print("processing numbers")
-        func(*args, **kyargs)
-        print("processing complete")
-
-    return wrapper
+# print(squares)
+# print(square_dict)
 
 
-lst = [1, 2, 3, 4, 5, 6]
+# # q.25
+# def check_even(func):
+#     def wrapper(*args, **kyargs):
+#         print("processing numbers")
+#         func(*args, **kyargs)
+#         print("processing complete")
+
+#     return wrapper
 
 
-@check_even
-def print_even_num(lst):
-    even_num = [num for num in lst if num % 2 == 0]
-    print("Even list:", even_num)
-    squ_dict = {num: num**2 for num in even_num}
-    print("Square dictionary:", squ_dict)
+# lst = [1, 2, 3, 4, 5, 6]
 
 
-print_even_num(lst)
+# @check_even
+# def print_even_num(lst):
+#     even_num = [num for num in lst if num % 2 == 0]
+#     print("Even list:", even_num)
+#     squ_dict = {num: num**2 for num in even_num}
+#     print("Square dictionary:", squ_dict)
+
+
+# print_even_num(lst)
+
+
+# q.3
+
+
+
+n = int(input())
+
+arr = list(int(input().split()))
+
+for i in range(n):
+    count = 0
+    for i in arr:
+        count += 1
+print(count)
+
