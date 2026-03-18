667df79366b73d3ea1a12881faff9f265721a318
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-18 11:19:06 +0530

    queue operation with correction

--- reverse_k_ele_from queuetostack.py
+++ reverse_k_ele_from queuetostack.py
@@ -1,31 +1,37 @@
-
-import queue
-
-
 class Queue:
-  def __init__(self):
-    self.queue = []
-    self.stack = []
-	
-  def qqueue(self, element):
-    self.queue.append(element)
-	
-	
-  def reverse_k_ele(self, k):
-    if k > len(self.queue):
-      return "k is greater than queue size"
-    for i in range(k):
-      self.stack.append(self.queue.pop(0))
-    while self.stack:
-      self.queue.append(self.stack.pop())
-  
+    def __init__(self):
+        self.queue = []
+        self.stack = []
+
+    def qqueue(self, element):
+        self.queue.append(element)
+
+    def reverse_k_ele(self, k):
+
+        if k > len(self.queue):
+            return "k greater than queue size"
+
+        # Step 1: push first k elements to stack
+        for i in range(k):
+            self.stack.append(self.queue.pop(0))
+
+        # Step 2: add stack elements back
+        while self.stack:
+            self.queue.append(self.stack.pop())
+
+        # Step 3: move remaining elements
+        for i in range(len(self.queue) - k):
+            self.queue.append(self.queue.pop(0))
+
+
 ob = Queue()
+
 ob.qqueue("a")
 ob.qqueue("b")
 ob.qqueue("c")
-print(ob.queue)
 
-print("ob.reverse_k_ele:", ob.reverse_k_ele(2))
+print("Original queue:", ob.queue)
+
+ob.reverse_k_ele(2)
+
 print("After reversing first 2 elements:", ob.queue)
-
-
