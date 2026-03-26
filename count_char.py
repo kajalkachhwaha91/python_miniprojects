dd181b2b99bf773799a29bf8fa921089426784ba
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-26 12:07:08 +0530

    armstrong number

--- count_char.py
+++ count_char.py
@@ -5,9 +5,4 @@ word = input("Enter a word: ")
 x = (input("Enter a character to count: "))
 
 
-res = 0
-for i in word:
-    if i == x:
-        res += 1
-    # break
-print(res)
+print(word.count(x))
