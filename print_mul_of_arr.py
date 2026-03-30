ed04cb9392311682270dd3ad45be650bef92777d
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2026-03-30 10:20:41 +0530

    calculating century

--- print_mul_of_arr.py
+++ print_mul_of_arr.py
@@ -1,5 +1,11 @@
-n = input()
-p =1
-for i in n:
-    p *= int(i)
-print(p)
+n = int(input())
+# p =1
+# for i in n:
+#     p *= int(i)
+# print(p)
+
+
+if n % 100 == 0:
+    print(n// 100)
+else:
+    print(n// 100 + 1)
